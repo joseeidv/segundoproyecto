e35e1ce029175f52b6322e87a4ae34739093fc83
jose <idiaazv@gmail.com>
2022-11-15 11:29:33 -0300

    cuartocommit(formulario completo funcionando)

--- src/test/java/pages/Login.java
+++ src/test/java/pages/Login.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Login {
 
@@ -16,11 +18,15 @@ public class Login {
     @FindBy(xpath = "//*[@id=\"imLogin\"]/form/div[3]/input")
     WebElement btnIngresar;
 
+    WebDriverWait webDriverWait;
+
     public Login(WebDriver webDriver){
         PageFactory.initElements(webDriver, this);
+        this.webDriverWait = new WebDriverWait(webDriver, 30);
     }
 
     public void ingresarUsuario(String usuario){
+        webDriverWait.until(ExpectedConditions.visibilityOf(inputUsuario));
         inputUsuario.sendKeys(usuario);
     }
 
--- src/test/java/testClasses/Logeo.java
+++ src/test/java/testClasses/Logeo.java
@@ -1,12 +1,13 @@
 package testClasses;
 
 import org.openqa.selenium.WebDriver;
+import pages.CargaInformacion;
 import pages.Login;
 
 public class Logeo {
 
     private Login login;
-    //private CargaInformacion cargaInformacion;
+    private CargaInformacion cargaInformacion;
     WebDriver webDriver;
 
     public Logeo(WebDriver webDriver){
@@ -15,10 +16,18 @@ public class Logeo {
 
     public void CasoLogin1(String usuario, String clave){
         login = new Login(webDriver);
-        //cargaInformacion = new CargaInformacion(webDriver);
+        cargaInformacion = new CargaInformacion(webDriver);
         login.ingresarUsuario(usuario);
         login.ingresarClave(clave);
         login.clickBtnIngresar();
-        //cargaInformacion.recuperarTitulo();
+        cargaInformacion.recuperarTitulo();
+        cargaInformacion.rellenarCampoTexto("Prueba");
+        cargaInformacion.rellenarCampoMail("prueba@gmail.com");
+        cargaInformacion.rellenarCampoAreaTexto("prueba texto largo");
+        cargaInformacion.rellenarFecha("26/02/2001");
+        cargaInformacion.rellenarLista("valor 3");
+        cargaInformacion.seleccionMultiple("1,3");
+        cargaInformacion.seleccionRadioButton(2);
+        cargaInformacion.clickBtnEnviar();
     }
 }
